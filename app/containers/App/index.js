8549072e883b7a2500d79dff60ab9ae1cee418fd
Rogelio Alberto <i@rog.mx>
2016-06-10 22:32:00 -0500

    Blank React App with sagas and reducers

---
+++ app/containers/App/index.js
@@ -0,0 +1,20 @@
+/*  App.react.js
+ *  This component is the skeleton around the actual pages, and should only
+ *  contain code that should be seen on all pages. (e.g. navigation bar)
+ */
+
+import React from 'react'
+
+export default class App extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.node
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,14 @@
+/* HomePage
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react'
+
+export default class HomePage extends React.Component {
+
+  render () {
+    return (
+      <h1>Hyperfox!</h1>
+    )
+  }
+}
