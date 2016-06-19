3ba56a3aada13803ee9de8241b17f405c2656f1d
Rogelio Alberto <i@rog.mx>
2016-06-18 22:32:00 -0500

    Responsive navbar

--- app/components/NavigationBar/index.js
+++ app/components/NavigationBar/index.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import {FormattedMessage} from 'react-intl'
-import classNames from 'classnames'
 
-import Logo from 'components/Icons/Logo'
-import messages from './messages'
+import classNames from 'classnames'
 import styles from './styles.css'
 
-export default class NavigationBar extends React.Component {
-  constructor (props) {
-    super(props)
-  }
+import {FormattedMessage} from 'react-intl'
+import messages from './messages'
 
-  render () {
-    return (
-      <div className='columns is-marginless'>
-        <div className={classNames('column', styles.NavigationBar__Logo)}>
-          <span><Logo className={styles.Logo} width={"65px"} height={"65px"} /></span>
-          <span className={styles.NavigationBar__Logo__Live}>/ Live</span>
-        </div>
-        <div className={classNames('column', styles.NavigationBar__Search)}>
+import Logo from 'components/Icons/Logo'
+
+export default function NavigationBar () {
+  return (
+    <nav className={classNames('nav', 'is-marginless', styles.NavigationBar)}>
+      <div className={classNames('nav-left', styles.NavigationBar__Logo)}>
+        <span className='nav-item'>
+          <Logo className={styles.Logo} width={"38px"} height={"38px"} />
+        </span>
+        <span className={classNames('nav-item', styles.NavigationBar__Logo__Live)}>/ Live</span>
+      </div>
+      <div className={classNames('nav-right', styles.NavigationBar__Search)}>
+        <div className='nav-item'>
           <p className={classNames('control', 'has-addons', styles.NavigationBar__Search__Input)}>
-            <input className='input is-normal' type='text' placeholder='Keywords' />
-            <a className='button is-normal is-info'>
+            <input className='input is-primary' type='text' placeholder='Keywords' />
+            <a className='button is-primary'>
               <span><FormattedMessage {...messages.searchMessage} /></span>
               <span className='icon'><i className='fa fa-search'></i></span>
             </a>
           </p>
         </div>
       </div>
-    )
-  }
+    </nav>
+  )
 }
